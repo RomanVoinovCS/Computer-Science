f37c0b5febd8713709f5841e5ae950619f037f7d
RomanVoinovCS <voinovr2909@student.laccd.edu>
2025-09-06 13:32:07 -0700

    Add files via upload

---
+++ main_Roman_Voinov.cpp
@@ -0,0 +1,33 @@
+// Roman Voinov
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    cout << "\nMy name is Roman Voinov" << endl;  
+
+    cout << "\nEnter the first number > ";
+    cin >> num1;
+
+    cout << "\nEnter the second number > ";
+    cin >> num2;
+
+    sum = num1 + num2; 
+
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+ My name is Roman Voinov
+
+ Enter the first number > 8
+
+ Enter the second number > 3
+
+ The sum of 8 and 3 is 11
+*/
